859833137df72355d6e2a67dacb5b8e7c0261bb3
sammymutahigicheru <sammymutahi12@gmail.com>
2020-11-25 09:05:15 +0300

    create favorites dao '

--- local/src/main/java/com/sammy/local/LocationDatabase.kt
+++ local/src/main/java/com/sammy/local/LocationDatabase.kt
@@ -1,2 +1,14 @@
 package com.sammy.local
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.sammy.local.dao.FavoriteDao
+import com.sammy.local.models.FavouriteEntity
+
+@Database(
+    entities = [FavouriteEntity::class],
+    version = 1, exportSchema = false
+)
+abstract class LocationDatabase : RoomDatabase() {
+    abstract fun favouritesDao():FavoriteDao
+}
