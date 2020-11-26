29c599c66888d6c0ed38e6853c3d1d88d88b0be7
sammymutahigicheru <sammymutahi12@gmail.com>
2020-11-26 09:53:34 +0300

    add network module

---
+++ app/src/main/java/com/sammy/location/di/NetworkModule.kt
@@ -0,0 +1,40 @@
+package com.sammy.location.di
+
+import com.sammy.data_remote.api.LocationApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+val networkModule = module {
+
+    single { provideStarWarsService(retrofit = get()) }
+
+    single { provideRetrofit(okHttpClient = get(), url = "https://swapi.dev/api/") }
+
+    single { provideOkHttpClient() }
+}
+
+internal fun provideOkHttpClient(): OkHttpClient {
+    val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    return OkHttpClient.Builder()
+        .connectTimeout(60L, TimeUnit.SECONDS)
+        .readTimeout(60L, TimeUnit.SECONDS)
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+}
+
+internal fun provideRetrofit(okHttpClient: OkHttpClient, url: String): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(url)
+        .client(okHttpClient)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+}
+
+internal fun provideStarWarsService(retrofit: Retrofit): LocationApiService =
+    retrofit.create(LocationApiService::class.java)
