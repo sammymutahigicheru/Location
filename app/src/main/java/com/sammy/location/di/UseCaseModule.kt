0d5060dd678b6eeac821e025d8afbfcc37ae52ff
sammymutahigicheru <sammymutahi12@gmail.com>
2020-11-26 11:50:24 +0300

    modify usecasemodule

--- app/src/main/java/com/sammy/location/di/UseCaseModule.kt
+++ app/src/main/java/com/sammy/location/di/UseCaseModule.kt
@@ -3,10 +3,11 @@ package com.sammy.location.di
 import com.sammy.domain.repository.ILocationRepository
 import com.sammy.domain.usecases.LocationUseCase
 import com.sammy.domain.usecases.LocationsBaseUseCase
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val useCaseModule = module {
-
+   single(named("location")) { provideLocationUseCase(get()) }
 }
 internal fun provideLocationUseCase(location:ILocationRepository): LocationsBaseUseCase =
     LocationUseCase(location)
---
+++ app/src/main/java/com/sammy/location/di/ViewModelModule.kt
@@ -0,0 +1,7 @@
+package com.sammy.location.di
+
+import org.koin.dsl.module
+
+val viewModelModule = module {
+
+}
