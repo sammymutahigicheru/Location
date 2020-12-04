3cdeb651e6aacbedac28d0f34b5425c36ab9e46e
sammymutahigicheru <sammymutahi12@gmail.com>
2020-12-04 15:44:05 +0300

    modify viewmodel test

--- app/src/test/java/com/sammy/location/viewmodels/MainActivityViewModelTest.kt
+++ app/src/test/java/com/sammy/location/viewmodels/MainActivityViewModelTest.kt
@@ -2,9 +2,15 @@ package com.sammy.location.viewmodels
 
 import android.os.Build
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
 import com.sammy.location.BaseViewModelTest
+import com.sammy.location.fakes.FakeGetLocationUseCase
+import com.sammy.location.utils.Data
 import com.sammy.location.utils.UiState
+import com.sammy.location.utils.observeOnce
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 
@@ -12,7 +18,24 @@ import org.robolectric.annotation.Config
 @Config(sdk = [Build.VERSION_CODES.P])
 @ExperimentalCoroutinesApi
 class MainActivityViewModelTest:BaseViewModelTest() {
-    override fun prepareViewModel(uiState: UiState) {
+    lateinit var viewModel:MainActivityViewModel
+    @Test
+    fun `given a body request parameter when search executed then return success state`() {
+        coroutineTestRule.dispatcher.runBlockingTest {
+            prepareViewModel(UiState.SUCCESS)
 
+            viewModel.executeGetLocation(Data.request)
+
+            advanceTimeBy(600)
+
+            viewModel.locationViewState.observeOnce{ state ->
+                Truth.assertThat(state.error).isNull()
+                Truth.assertThat(state.locationResults).isNotEmpty()
+            }
+        }
+    }
+    override fun prepareViewModel(uiState: UiState) {
+        val locationsBaseUseCase = FakeGetLocationUseCase(uiState)
+        viewModel = MainActivityViewModel(locationsBaseUseCase)
     }
 }
